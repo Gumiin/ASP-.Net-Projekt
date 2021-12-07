234ae0912a9b5a80c5a3829c85edd11544ad4100
Gumiin <jakub.szlachetka@microsoft.wsei.edu.pl>
2021-12-07 09:07:30 +0100

    2

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab_5_2.Models
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : 
+            base(options) { }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
---
+++ Models/Issue.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_5_2.Models
+{
+    public class Issue
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+        public int Category { get; set; }
+    }
+}
