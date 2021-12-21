906143e6749172298538259c1c5bf52c989701ab
Gumiin <jakub.szlachetka@microsoft.wsei.edu.pl>
2021-12-21 13:03:02 +0100

    3

--- Controllers/ApiBookController.cs
+++ Controllers/ApiBookController.cs
@@ -29,11 +29,56 @@ namespace Lab_5_2.Controllers
             };
         }
         [HttpPost]
-        public IActionResult AddBook ([FromBody] Book)
+        public IActionResult AddBook([FromBody] Book)
         {
             //dodanie obiektu do repozytorium
             book.Id = 5;
             return new CreatedResult($"/api/books/{book.Id}", book);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook()
+        {
+            //pobieranie obiektu z repozytorium o danym id
+            if (id < 5 && Id > 0)
+            {
+                return new OkObjectResult(new Book() { Id = id, Title = "tytu³" });
+            }
+            else
+            {
+                return new NotFoundResult();
+            }
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Book> EditBook(int id, [FromBody] Book book)
+        {
+            //wywo³anie metody z repozytorium zmieniaj¹cej wartoœci w obiekcie
+            //testujemy czy obiekt jest inny od null
+            if (id < 5 && id > 0)
+            {
+
+
+                return new OkObjectResult(book);
+            }
+            else
+            {
+
+                return NotFound();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            //wywo³anie metody z repozytorium zmieniaj¹cej wartoœci w obiekcie
+            //testujemy czy obiekt jest inny od null
+            if (id < 5 && id > 0)
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
