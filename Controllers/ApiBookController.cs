cf005fc066f7aa367e329989231022547c5bb210
Gumiin <jakub.szlachetka@microsoft.wsei.edu.pl>
2022-01-04 11:15:35 +0100

    1

--- Controllers/ApiBookController.cs
+++ Controllers/ApiBookController.cs
@@ -1,3 +1,4 @@
+using Lab_5_2.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -11,38 +12,34 @@ namespace Lab_5_2.Controllers
     [ApiController]
     public class ApiBookController : ControllerBase
     {
+        private ICRUDBookRepository books;
+
+        public ApiBookController(ICRUDBookRepository books)
+        {
+            this.books = books;
+        }
+
         //deklaracja zmiennej repozytorium
         [HttpGet]
 
-        public List<Book> GetBooks()
+        public IList<Book> GetBooks()
         {
-            return new List<Book>()
-            {
-                new Book()
-                {
-                    Title="AAAA"
-                },
-                new Book()
-                {
-                    Title="BBBB"
-                }
-            };
+            return books.FindAll();
         }
         [HttpPost]
-        public IActionResult AddBook([FromBody] Book)
+        public ActionResult<Book> AddBook([FromBody] Book book)
         {
-            //dodanie obiektu do repozytorium
-            book.Id = 5;
+            Book book1 = books.Add(book);
             return new CreatedResult($"/api/books/{book.Id}", book);
         }
 
         [HttpGet("{id}")]
         public ActionResult<Book> GetBook()
         {
-            //pobieranie obiektu z repozytorium o danym id
-            if (id < 5 && Id > 0)
+            Book book = books.FindById(id);
+            if (book != null)
             {
-                return new OkObjectResult(new Book() { Id = id, Title = "tytu³" });
+                return new OkObjectResult(book);
             }
             else
             {
---
+++ Lab_5_2_test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Lab_5_2_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
